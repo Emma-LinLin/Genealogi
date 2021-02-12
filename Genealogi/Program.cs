1dfd875be38acd2e601abd6f6cf3c7724750740b
emmalinnearoos@gmail.com <emmalinnearoos@gmail.com>
2021-02-12 12:09:28 +0100

    Add project files.

---
+++ Genealogi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Genealogi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
