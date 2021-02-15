39160ceba2945d642b3f443d2b9a82181803a1f6
emmalinnearoos@gmail.com <emmalinnearoos@gmail.com>
2021-02-15 15:29:21 +0100

    Added methods DoesDatabaseExist/DropDatabase and comments

--- Genealogi/FamilyMembers/Person.cs
+++ Genealogi/FamilyMembers/Person.cs
@@ -12,21 +12,19 @@ namespace Genealogi.FamilyMembers
         public int BirthDate { get; set; }
         public int DeathDate { get; set; }
         public string City { get; set; }
-        public int Mother { get; set; }
-        public int Father { get; set; }
+        public int Mother { get; set; } = default;
+        public int Father { get; set; } = default;
         public Person()
         {
 
         }
-        public Person(string firstName, string lastName, int birthDate, int deathDate, string city, int mother, int father)
+        public Person(string firstName, string lastName, int birthDate, int deathDate, string city)
         {
             FirstName = firstName;
             LastName = lastName;
             BirthDate = birthDate;
             DeathDate = deathDate;
             City = city;
-            Mother = mother;
-            Father = father;
         }
     }
 }
--- Genealogi/Programlogic/FamilyTree.cs
+++ Genealogi/Programlogic/FamilyTree.cs
@@ -17,6 +17,12 @@ namespace Genealogi.Programlogic
             var db = new SqlDatabase();
             db.DatabaseName = DatabaseName;
 
+            bool exists = DoesDatabaseExist();
+            if(exists)
+            {
+                DropDatabase();
+            }
+
             CreateDatabase();
             CreateTable();
             GenerateSampleData();
@@ -34,12 +40,55 @@ namespace Genealogi.Programlogic
 
             SearchPeople();
         }
+
+        /// <summary>
+        /// Checks if database already exists.
+        /// </summary>
+        /// <returns>Boolean, true or false</returns>
+        private bool DoesDatabaseExist()
+        {
+            var db = new SqlDatabase();
+            db.DatabaseName = "Master";
+
+            string parameterName = "@name";
+            string parameterValue = "Genealogi";
+            var DataTable = db.GetDataTable("SELECT * FROM sys.databases WHERE name=@name", parameterName, parameterValue);
+
+            if (DataTable.Rows.Count < 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Drops database and closes connections.
+        /// </summary>
+        private void DropDatabase()
+        {
+            var db = new SqlDatabase();
+            db.DatabaseName = "Master";
+
+            // Database is being used issue - https://stackoverflow.com/a/20569152/15032536
+            db.ExecuteSQL(" alter database [Genealogi] set single_user with rollback immediate");
+
+            db.ExecuteSQL("DROP DATABASE Genealogi");
+        }
+
+        /// <summary>
+        /// Creates new database with database name Genealogi.
+        /// </summary>
         private void CreateDatabase()
         {
             var db = new SqlDatabase();
 
             db.ExecuteSQL("CREATE DATABASE Genealogi;");
         }
+
+        /// <summary>
+        /// Creates table within database Genealogi.
+        /// </summary>
         private void CreateTable()
         {
             var db = new SqlDatabase();
@@ -56,6 +105,9 @@ mother int,
 father int);");
         }
 
+        /// <summary>
+        /// Generates sample data of Person and creates them seperately in database with the help of an foreach loop.
+        /// </summary>
         public void GenerateSampleData()
         {
             var db = new SqlDatabase();
@@ -63,25 +115,25 @@ father int);");
 
             ListOfPersons = new List<Person>()
             {
-                new Person("Finn", "Linné", 1835, 1928, "Brindleton Bay", 0, 0),
-                new Person("Belinda", "Barrera", 1835, 1928, "Brindleton Bay", 0, 0),
-                new Person("Collin", "Barrera", 1881, 1974, "Brindleton Bay", 0, 0),
-                new Person("Fiona", "Barrera", 1882, 1975, "Brindleton Bay", 0, 0),
-                new Person("Analisa", "Vasquez", 1840, 1933, "Salvadoradia", 0, 0),
-                new Person("Charlie", "Vasquez", 1895, 1988, "Salvadoradia", 0, 0),
-                new Person("Eric", "Eyna", 1840, 1933, "Willow Creek", 0, 0),
-                new Person("Christina", "Eyna", 1840, 1933, "Willow Creek", 0, 0),
-                new Person("Celine", "Eyna", 1884, 1977, "Brindleton Bay", 0, 0),
-                new Person("Emanuel", "Eyna", 1924, 0, "Brindleton Bay", 0, 0),
-                new Person("Noel", "Michaud", 1924, 0, "Willow Creek", 0, 0),
-                new Person("Valentina", "Salas", 1924, 0, "Windenburg", 0, 0),
-                new Person("Paulina", "Salas", 1884, 1977, "Windenburg", 0, 0),
-                new Person("Tim", "Salas", 1964, 0, "Windenburg", 0, 0),
-                new Person("Selina", "Michaud", 1954, 0, "Willow Creek", 0, 0),
-                new Person("Hollie", "Michaud", 1964, 0, "Willow Creek", 0, 0),
-                new Person("Silas", "Michaud", 1970, 0, "Willow Creek", 0, 0),
-                new Person("Mischa", "Michaud", 1970, 0, "Willow Creek", 0, 0),
-                new Person("Brevbäraren", "Michelangelo", 1924, 0, "Willow Creek", 0, 0),
+                new Person("Finn", "Linné", 1835, 1928, "Brindleton Bay"),
+                new Person("Belinda", "Barrera", 1835, 1928, "Brindleton Bay"),
+                new Person("Collin", "Barrera", 1881, 1974, "Brindleton Bay"),
+                new Person("Fiona", "Barrera", 1882, 1975, "Brindleton Bay"),
+                new Person("Analisa", "Vasquez", 1840, 1933, "Salvadoradia"),
+                new Person("Charlie", "Vasquez", 1895, 1988, "Salvadoradia"),
+                new Person("Eric", "Eyna", 1840, 1933, "Willow Creek"),
+                new Person("Christina", "Eyna", 1840, 1933, "Willow Creek"),
+                new Person("Celine", "Eyna", 1884, 1977, "Brindleton Bay"),
+                new Person("Emanuel", "Eyna", 1924, 0, "Brindleton Bay"),
+                new Person("Noel", "Michaud", 1924, 0, "Willow Creek"),
+                new Person("Valentina", "Salas", 1924, 0, "Windenburg"),
+                new Person("Paulina", "Salas", 1884, 1977, "Windenburg"),
+                new Person("Tim", "Salas", 1964, 0, "Windenburg"),
+                new Person("Selina", "Michaud", 1954, 0, "Willow Creek"),
+                new Person("Hollie", "Michaud", 1964, 0, "Willow Creek"),
+                new Person("Silas", "Michaud", 1970, 0, "Willow Creek"),
+                new Person("Mischa", "Michaud", 1970, 0, "Willow Creek"),
+                new Person("Brevbäraren", "Michelangelo", 1924, 0, "Willow Creek"),
 
             };
 
@@ -103,6 +155,16 @@ father int);");
             SetRelations("Mischa", "Michaud", "Noel", "Michaud", "Emanuel", "Eyna");
 
         }
+
+        /// <summary>
+        /// Sets the relation of the Person
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="motherFirstName"></param>
+        /// <param name="motherLastName"></param>
+        /// <param name="fatherFirstName"></param>
+        /// <param name="fatherLastName"></param>
         public void SetRelations(string firstName, string lastName, string motherFirstName = "", string motherLastName = "", string fatherFirstName = "", string fatherLastName = "")
         {
             var db = new SqlDatabase();
@@ -112,15 +174,12 @@ father int);");
             var fatherID = db.GetPersonId(fatherFirstName, fatherLastName);
             var childID = db.GetPersonId(firstName, lastName);
 
-            UpdateRelations(childID, motherID, fatherID);
-
-        }
-        public void UpdateRelations(int childID, int motherID = 0, int fatherID = 0)
-        {
-            var db = new SqlDatabase();
-            db.DatabaseName = DatabaseName;
             db.ExecuteSQL($"UPDATE Persons SET mother={motherID},father={fatherID} WHERE Id={childID};");
         }
+
+        /// <summary>
+        /// Prints the current table in Database.
+        /// </summary>
         public void PrintTable()
         {
             var db = new SqlDatabase();
@@ -136,6 +195,9 @@ father int);");
                 }
             }
         }
+        /// <summary>
+        /// Allows you to search for a Person by entering first- or last name.
+        /// </summary>
         public void SearchPeople()
         {
             Console.WriteLine();
@@ -159,6 +221,10 @@ father int);");
             ListRelations(searchedPerson);
 
         }
+        /// <summary>
+        /// Lists relatives based of the Person. 
+        /// </summary>
+        /// <param name="searchedPerson"></param>
         public void ListRelations(Person searchedPerson)
         {
             var db = new SqlDatabase();
@@ -195,26 +261,38 @@ father int);");
                 }
             }
 
-            GetSiblings(searchedPerson.Mother, searchedPerson.Father);
+            GetSiblings(searchedPerson.Mother, searchedPerson.Father, searchedPerson.Id);
             GetChildren(searchedPerson.Id);
         }
-        public void GetSiblings(int motherID, int fatherID)
+
+        /// <summary>
+        /// Lists Persons with the same mother- and father ID as the search result.
+        /// </summary>
+        /// <param name="motherID"></param>
+        /// <param name="fatherID"></param>
+        /// <param name="personID"></param>
+        public void GetSiblings(int motherID, int fatherID, int personID)
         {
             var db = new SqlDatabase();
             db.DatabaseName = DatabaseName;
 
 
-            var respons = db.GetDataTable(@$"SELECT * FROM Persons WHERE mother={motherID} AND father={fatherID}");
+            var respons = db.GetDataTable(@$"SELECT * FROM Persons WHERE NOT Id={personID} AND mother={motherID} AND father={fatherID}");
 
             if (respons != null)
             {
                 foreach (DataRow row in respons.Rows)
                 {
-                    Console.Write("Sibling: ");
-                    Console.WriteLine($"{row["firstName"]} {row["lastName"]}");
+                        Console.Write("Sibling: ");
+                        Console.WriteLine($"{row["firstName"]} {row["lastName"]}");
                 }
             }
         }
+
+        /// <summary>
+        /// Lists Persons where the ID matches the mother- or father ID. 
+        /// </summary>
+        /// <param name="id"></param>
         public void GetChildren(int id)
         {
             var db = new SqlDatabase();
